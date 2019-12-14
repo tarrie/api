773e1220fca9798b71b93f493e3adb4a65009136
Jidé Anene <dominicanene@gmail.com>
2019-12-14 16:13:29 -0600

    added swagger integration

--- TarrieRESTfulAPI/target/api-1.0-SNAPSHOT/WEB-INF/classes/io/tarrie/api/Search.java
+++ TarrieRESTfulAPI/target/api-1.0-SNAPSHOT/WEB-INF/classes/io/tarrie/api/Search.java
@@ -1,5 +1,6 @@
 package io.tarrie.api;
 
+import io.swagger.annotations.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -9,11 +10,24 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 
+@Api(tags="/search")
+@SwaggerDefinition(tags = {
+        @Tag(name = "Swagger Resource", description = "Write description here")
+})
 @Path("/search")
 public class Search {
 
     /** when testing, this is reachable at http://localhost:8080/api/search?query=hello */
     @GET
+    @ApiOperation(
+            value = "Lists all employees",
+            notes = "Lists all employees"
+    )
+    @ApiResponses(value= {
+            @ApiResponse(code = 200, message = "Successful retrieval of employees"),
+            @ApiResponse(code = 404, message = "Employee records not found"),
+            @ApiResponse(code = 500, message = "Internal servererror")
+    })
     public Response getMsg(@QueryParam("query") String q) throws IOException {
         JSONArray results = new JSONArray();
         results.put("hello world!");
